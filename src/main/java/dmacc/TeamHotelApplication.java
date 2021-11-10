b38567dd7a094ebca56ebed6e1f31c63f58592b9
gboothroyd <89821743+gboothroyd@users.noreply.github.com>
2021-11-09 18:23:45 -0600

    Still setting up the project
    Just more setup and starting my end of the project

---
+++ src/main/java/dmacc/TeamHotelApplication.java
@@ -0,0 +1,25 @@
+package dmacc;
+
+/**
+ * @author booth - gboothroyd@dmacc.edu
+ *CIS175 - Fall 2021
+ * Nov 9, 2021
+ */
+public class TeamHotelApplication {
+
+	/**
+	 * 
+	 */
+	public TeamHotelApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/dmacc/beans/Hotel.java
@@ -0,0 +1,17 @@
+package dmacc.beans;
+
+/**
+ * @author booth - gboothroyd@dmacc.edu
+ *CIS175 - Fall 2021
+ * Nov 9, 2021
+ */
+public class Hotel {
+
+	/**
+	 * 
+	 */
+	public Hotel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
