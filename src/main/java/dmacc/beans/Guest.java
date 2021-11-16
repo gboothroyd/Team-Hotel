76516ddb04e73da8de19e029a480bcddd805cb1e
Dylan <djthomas4@dmacc.edu>
2021-11-15 18:21:21 -0600

    Changed Hotel to Room, Added Guest and Reservation Classes

---
+++ src/main/java/dmacc/beans/Guest.java
@@ -0,0 +1,92 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * @author Dylan Thomas - djthomas4
+ * CIS175 - Fall 2021
+ * Nov 14, 2021
+ */
+
+@Entity
+public class Guest {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String fName;
+	private String lName;
+	private String phone;
+	private String paymentMethod;
+	//private String email;
+	//Reservation reserved;
+	
+	
+	
+	public Guest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Guest(String fName, String lName, String phone, String paymentMethod) {
+		super();
+		this.fName = fName;
+		this.lName = lName;
+		this.phone = phone;
+		this.paymentMethod = paymentMethod;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	@Override
+	public String toString() {
+		return "Guest [id=" + id + ", fName=" + fName + ", lName=" + lName + ", phone=" + phone + ", paymentMethod="
+				+ paymentMethod + "]";
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/dmacc/beans/Reservation.java
@@ -0,0 +1,107 @@
+package dmacc.beans;
+
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ * @author Dylan Thomas - djthomas4
+ * CIS175 - Fall 2021
+ * Nov 14, 2021
+ */
+
+@Entity
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@OneToOne(cascade=CascadeType.ALL)
+	private Guest guest;
+	@OneToOne(cascade=CascadeType.ALL)
+	private Room room;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private int numOccupants;
+	
+	public Reservation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reservation(Guest guest, Room room, LocalDate startDate, LocalDate endDate, int numOccupants) {
+		super();
+		this.guest = guest;
+		this.room = room;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.numOccupants = numOccupants;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Guest getGuest() {
+		return guest;
+	}
+
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public int getNumOccupants() {
+		return numOccupants;
+	}
+
+	public void setNumOccupants(int numOccupants) {
+		this.numOccupants = numOccupants;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", guest=" + guest + ", room=" + room + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", numOccupants=" + numOccupants + "]";
+	}
+	
+	
+
+
+	
+
+}
