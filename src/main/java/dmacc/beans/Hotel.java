73f4678f6f2a212de76245de1eaaf40d70877a71
Dylan <djthomas4@dmacc.edu>
2021-11-15 18:41:33 -0600

    Revert "Changed Hotel to Room, Added Guest and Reservation Classes"
    This reverts commit 76516ddb04e73da8de19e029a480bcddd805cb1e.

--- src/main/java/dmacc/beans/Hotel.java
+++ src/main/java/dmacc/beans/Hotel.java
@@ -5,46 +5,44 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
- * @author booth - gboothroyd@dmacc.edu CIS175 - Fall 2021 Nov 9, 2021
+ * @author booth - gboothroyd@dmacc.edu
+ *CIS175 - Fall 2021
+ * Nov 9, 2021
  */
 
 @Entity
-public class Room {
+public class Hotel {
 	@Id
 	@GeneratedValue
 	private long id;
-	private String roomNum;
-	private String availability;
-	private double roomPrice;
-	private String roomSize;
-
-	public Room() {
-		super();
+	private String name;
+private String availability;
+private double roomPrice;
+private String roomSize;
+	public Hotel() {
 		// TODO Auto-generated constructor stub
 	}
-
 	/**
 	 * @param availability
 	 * @param roomPrice
 	 * @param roomSize
 	 */
-	public Room(String roomNum, String availability, double roomPrice, String roomSize) {
+	public Hotel(String name, String availability, double roomPrice, String roomSize) {
 		super();
-		this.roomNum = roomNum;
+		this.name = name;
 		this.availability = availability;
 		this.roomPrice = roomPrice;
 		this.roomSize = roomSize;
-
+		
 	}
-
 	/**
 	 * @return the availability
 	 */
-
+	
+	
 	public String getAvailability() {
 		return availability;
 	}
-
 	/**
 	 * @return the id
 	 */
@@ -55,64 +53,59 @@ public class Room {
 	public long getId() {
 		return id;
 	}
-
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	/**
 	 * @return the name
 	 */
-	public String getRoomNum() {
-		return roomNum;
+	public String getName() {
+		return name;
 	}
-
 	/**
 	 * @param name the name to set
 	 */
-	public void setRoomNum(String roomNum) {
-		this.roomNum = roomNum;
+	public void setName(String name) {
+		this.name = name;
 	}
-
 	public void setAvailability(String availability) {
 		this.availability = availability;
 	}
-
 	/**
 	 * @return the roomPrice
 	 */
 	public double getRoomPrice() {
 		return roomPrice;
 	}
-
 	/**
 	 * @param roomPrice the roomPrice to set
 	 */
 	public void setRoomPrice(double roomPrice) {
 		this.roomPrice = roomPrice;
 	}
-
 	/**
 	 * @return the roomSize
 	 */
 	public String getRoomSize() {
 		return roomSize;
 	}
-
 	/**
 	 * @param roomSize the roomSize to set
 	 */
 	public void setRoomSize(String roomSize) {
 		this.roomSize = roomSize;
 	}
-
 	@Override
 	public String toString() {
-		return "Room [id=" + id + ", roomNum" + roomNum + ", availability=" + availability + ", roomPrice=" + roomPrice + ", roomSize="
+		return "Hotel [id=" + id + ", availability=" + availability + ", roomPrice=" + roomPrice + ", roomSize="
 				+ roomSize + "]";
 	}
+	
+	
+	
+	
 
 }
