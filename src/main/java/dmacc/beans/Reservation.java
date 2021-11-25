218ac2e15046033c33c10c34496b4a9d74850b01
CS-41 <66191434+CS-41@users.noreply.github.com>
2021-11-24 19:11:55 -0600

    Trying again...
    The web changes.

--- src/main/java/dmacc/beans/Reservation.java
+++ src/main/java/dmacc/beans/Reservation.java
@@ -25,13 +25,8 @@ public class Reservation {
 	private Guest guest;
 	@OneToOne(cascade=CascadeType.ALL)
 	private Room room;
-	
-	private String startDate;
-	private String endDate;
-	
-	//Date changed to String to facilitate "date" in html. EA	
-	//private LocalDate startDate;
-	//private LocalDate endDate;
+	private LocalDate startDate;
+	private LocalDate endDate;
 	private int numOccupants;
 
 	public Reservation() {
@@ -39,7 +34,7 @@ public class Reservation {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Reservation(Guest guest, Room room, String startDate, String endDate, int numOccupants) {
+	public Reservation(Guest guest, Room room, LocalDate startDate, LocalDate endDate, int numOccupants) {
 		super();
 		this.guest = guest;
 		this.room = room;
@@ -72,19 +67,19 @@ public class Reservation {
 		this.room = room;
 	}
 
-	public String getStartDate() {
+	public LocalDate getStartDate() {
 		return startDate;
 	}
 
-	public void setStartDate(String startDate) {
+	public void setStartDate(LocalDate startDate) {
 		this.startDate = startDate;
 	}
 
-	public String getEndDate() {
+	public LocalDate getEndDate() {
 		return endDate;
 	}
 
-	public void setEndDate(String endDate) {
+	public void setEndDate(LocalDate endDate) {
 		this.endDate = endDate;
 	}
 
--- src/main/java/dmacc/controller/WebControllerGary.java
+++ src/main/java/dmacc/controller/WebControllerGary.java
@@ -1,144 +1,24 @@
 package dmacc.controller;
 
-import java.util.ArrayList;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDate;
-//import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import dmacc.beans.Guest;
+
 import dmacc.beans.Reservation;
-import dmacc.service.GuestService;
-//import dmacc.beans.Room;
-//import dmacc.repository.GuestRepository;
-//import dmacc.repository.ReservationRepository;
-//import dmacc.repository.RoomRepository;
-import dmacc.service.ReservationService;
+import dmacc.beans.Room;
+import dmacc.repository.GuestRepository;
+import dmacc.repository.ReservationRepository;
+import dmacc.repository.RoomRepository;
 
 /**
  * @author booth - gboothroyd@dmacc.edu CIS175 - Fall 2021 Nov 10, 2021
  */
 @Controller
 public class WebControllerGary {
-	
-	@Autowired
-	private ReservationService resService;
-	@Autowired
-	private GuestService guestService;
-	
-    @GetMapping("/")
-    public String viewHomePage(Model model) {
-    	return findPaginated(1, "guest", "asc", model);
-    }
-    
-	@GetMapping("/newReservation")
-	public String newReservation(Model model) {
-		Reservation reservation = new Reservation();
-		model.addAttribute("reservation", reservation);
-		return "newreservation";
-	}
-	
-	@PostMapping("/saveReservation")
-	public String saveReservation(@ModelAttribute("reservation") Reservation reservation) {
-		resService.saveReservation(reservation);
-		return "redirect:/";
-	}
-	
-	@GetMapping("/editReservation/{id}")
-	public String editReservationById(@PathVariable ( value = "id") long id, Model model) {
-		Reservation reservation = resService.getReservationById(id);
-		model.addAttribute("reservation", reservation);
-		return "editreservation";
-	}
-	
-	@GetMapping("/deleteReservation/{id}")
-	public String deleteReservationById(@PathVariable (value = "id") long id) {
-		this.resService.deleteReservationById(id);
-		return "redirect:/";
-	}
-	
-	
-    @GetMapping("/viewGuests")
-    public String viewGuestHomePage(Model model) {
-    	return findGuestPaginated(1, "lName", "asc", model);
-    }
-    
-	@GetMapping("/editGuest/{id}")
-	public String editGuestById(@PathVariable ( value = "id") long id, Model model) {
-		Guest guest = guestService.getGuestById(id);
-		model.addAttribute("guest", guest);
-		return "editguest";
-	}
-	
-	@PostMapping("/saveGuest")
-	public String saveGuest(@ModelAttribute("guest") Guest guest) {
-		guestService.saveGuest(guest);
-		return "redirect:/viewGuests";
-	}
-	
-	@GetMapping("/deleteGuest/{id}")
-	public String deleteGuestById(@PathVariable (value = "id") long id) {
-		this.guestService.deleteGuestById(id);
-		return "redirect:/viewGuests";
-	}
-
-	@GetMapping("/guestPage/{pageNo}")
-	public String findGuestPaginated(@PathVariable (value = "pageNo") int pageNo, 
-			@RequestParam("sortField") String sortField,
-			@RequestParam("sortDir") String sortDir,
-			Model model) {
-		int pageSize = 5;
-		
-		Page<Guest> page = guestService.findGuestPaginated(pageNo, pageSize, sortField, sortDir);
-		List<Guest> listGuests = page.getContent();
-		
-		model.addAttribute("currentPage", pageNo);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("totalItems", page.getTotalElements());
-		
-		model.addAttribute("sortField", sortField);
-		model.addAttribute("sortDir", sortDir);
-		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-		
-		model.addAttribute("listGuests", listGuests);
-		return "guesthomepage";
-	}
-	
-	
-	@GetMapping("/page/{pageNo}")
-	public String findPaginated(@PathVariable (value = "pageNo") int pageNo, 
-			@RequestParam("sortField") String sortField,
-			@RequestParam("sortDir") String sortDir,
-			Model model) {
-		int pageSize = 5;
-		
-		Page<Reservation> page = resService.findPaginated(pageNo, pageSize, sortField, sortDir);
-		List<Reservation> listReservations = page.getContent();
-		
-		model.addAttribute("currentPage", pageNo);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("totalItems", page.getTotalElements());
-		
-		model.addAttribute("sortField", sortField);
-		model.addAttribute("sortDir", sortDir);
-		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-		
-		model.addAttribute("listReservations", listReservations);
-		return "homepage";
-	}
-		
-	
-	/*
 	@Autowired
 	RoomRepository repo;
 	
@@ -206,6 +86,6 @@ public class WebControllerGary {
 	reservedrepo.save(r);
 	return viewAllReservations(model);
 	}
-*/
+
 
 }
